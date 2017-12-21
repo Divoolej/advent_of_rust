b01e674bb9da4d8ae5f199c1a4a351e162d0052d
Wojciech Olejnik <divoolej@gmail.com>
2017-12-21 20:30:09 +0100

    WIP year 2015 day 2

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ fn main() {
     println!("| Day 1: ");
     println!("| - A) {}", year_2015::day_1::solve_a());
     println!("| - B) {}", year_2015::day_1::solve_b());
+    println!("| Day 2: ");
+    println!("| - A) {}", year_2015::day_2::solve_a());
+    println!("| - B) {}", year_2015::day_2::solve_b());
     // 2016
     println!("- Year 2016:");
     println!("| Day 1: ");
---
+++ src/year_2015/day_2/mod.rs
@@ -0,0 +1,10 @@
+mod a;
+mod b;
+
+pub fn solve_a() -> String {
+    a::solve()
+}
+
+pub fn solve_b() -> String {
+    b::solve()
+}
