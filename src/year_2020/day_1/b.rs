c53f3dc8a91845ad113e3b731482f66d5c9ce484
Wojciech Olejnik <divoolej@gmail.com>
2020-12-01 19:34:25 +0100

    Add whitespaces

--- src/year_2020/day_1/b.rs
+++ src/year_2020/day_1/b.rs
@@ -13,8 +13,10 @@ pub fn solve(input_dir: &str) -> String {
 
   for (idx_max, &max) in numbers.iter().enumerate().rev() {
     if max >= 2020 { break; }
+
     for (idx_min, min) in numbers[..idx_max].iter().enumerate() {
       if min + max >= 2020 { break; }
+
       for middle in numbers[(idx_min + 1)..idx_max].iter() {
         if min + middle + max == 2020 { return (min * middle * max).to_string(); }
         if min + middle + max > 2020 { break; }
